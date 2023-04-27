c4ed42d9b93d7088223e4d27b176b6f7e94ddba0
example <erennkazan@gmail.com>
2023-04-27 23:22:31 +0300

    herosection fixed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,27 @@
+
+import Slider from 'react-slick';
+
 export default function HeroSection(){
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+
     return(
-        <div>
-            hero section
+        <div className="">
+            <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+          <h3>2</h3>
+          </div>
+        </Slider>
         </div>
     )
 }
