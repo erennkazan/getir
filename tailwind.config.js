3fd9238952eb298f99db5e654494449a3dd1437d
example <erennkazan@gmail.com>
2023-05-02 23:11:39 +0300

    herosection fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       colors:{
         'brand-color': '#4C3398',
         'primary-brand-color': '#5d3ebc',
-        'secondary-brand-color': '#7849f7'
+        'secondary-brand-color': '#7849f7',
+        'brand-yellow': '#ffd300'
       }
     },
     variants: {
