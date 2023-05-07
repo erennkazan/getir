c2289584d3a7c0e7ef6ca2e93179363aafaf906e
eren kazan <erennkazan@gmail.com>
2023-05-08 00:54:04 +0300

    campaigns slide added

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -2,6 +2,37 @@ import { useEffect, useState } from "react";
 import Slider from 'react-slick';
 import Banners from 'api/banners.json';
 import Title from 'components/ui/Title';
+import {IoIosArrowBack , IoIosArrowForward} from 'react-icons/io'
+
+function NextBtn(props) {
+  const { className, style, onClick } = props;
+  return(
+    <button className="{className} style={style} onClick={onClick}">
+       <IoIosArrowForward/>
+    </button>
+  ) 
+}
+function PrevBtn(props) {
+  const { className, style, onClick } = props;
+  return (
+    <button className="{className} style={style} onClick={onClick}">
+      <IoIosArrowBack/>
+    </button> 
+  ) 
+    
+}
+
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 
 export default function Campaigns(){
 
@@ -15,23 +46,24 @@ export default function Campaigns(){
         dots: false,
         infinite: true,
         speed: 500,
-        arrows: false,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         speed: 500,
         autoplaySpeed: 3500,
-        cssEase: "linear"
+        cssEase: "linear",
+        nextArrow: <NextBtn />,
+        prevArrow: <PrevBtn />
       };
  
     return(
         <div className="container mx-auto py-8">
             <Title>Kampanyalar</Title>
-            <Slider {...settings}>
+            <Slider className="-mx-2" {...settings}>
               {banners.length && banners.map((banner,index) => (
                 <div key={banner.id}>
                     <picture className="block px-2">
-                      <img src={banner.image}/>
+                      <img src={banner.image} className="rounded-lg"/>
                     </picture>
                     
                 </div>
