7e0fabf7c029fd96b1c7145b9c26a8d903020284
eren kazan <erennkazan@gmail.com>
2023-05-13 21:02:52 +0300

    responsive complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,114 +1,113 @@
-import Menu from 'components/ui/Menu';
-import {FaFacebook,FaTwitter,FaInstagram} from 'react-icons/fa'
+import Menu from 'components/ui/Menu'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
 import { BiGlobe } from 'react-icons/bi'
 
-export default function Footer(){
+export default function Footer() {
 
     const menus = [
         {
-            title:'Getir\'i Keşfedin',
-            items:[
+            title: 'Getir\'i Keşfedin',
+            items: [
                 {
-                    title: 'Hakkımızda'  
+                    title: 'Hakkımızda'
                 },
                 {
-                    title: 'Kariyer'  
+                    title: 'Kariyer'
                 },
                 {
-                    title: 'Teknoloji Kariyerleri'  
+                    title: 'İletişim'
                 },
                 {
-                    title: 'İletişim'  
+                    title: 'COVID-19 Duyuru'
                 },
                 {
-                    title: 'Sosyal Sorumluluk Projeleri'  
-                },
-            ]   
+                    title: 'Sosyal Sorumluluk Projeleri'
+                }
+            ]
         },
         {
-            title:'Yardıma mı ihtiyacınız var?',
-            items:[
+            title: 'Yardıma mı ihtiyacınız var?',
+            items: [
                 {
-                    title: 'Sıkça Sorulan Sorular'  
+                    title: 'Hakkımızda'
                 },
                 {
-                    title: 'Kişisel Verilerin Korunması'  
+                    title: 'Kariyer'
                 },
                 {
-                    title: 'Gizlilik Politikası'  
+                    title: 'İletişim'
                 },
                 {
-                    title: 'Kullanım Koşulları'  
+                    title: 'COVID-19 Duyuru'
                 },
                 {
-                    title: 'Çerez Politikası'  
-                },
-            ]   
+                    title: 'Sosyal Sorumluluk Projeleri'
+                }
+            ]
         },
         {
-            title:'İş Ortağımız Olun',
-            items:[
+            title: 'İş Ortağımız Olun',
+            items: [
                 {
-                    title: 'Bayimiz Olun'  
+                    title: 'Hakkımızda'
                 },
                 {
-                    title: 'Deponuzu Kiralayın'  
+                    title: 'Kariyer'
                 },
                 {
-                    title: 'GetirYemek Restoranı Olun'  
+                    title: 'İletişim'
                 },
                 {
-                    title: 'GetirÇarşı İşletmesi Olun'  
+                    title: 'COVID-19 Duyuru'
                 },
                 {
-                    title: 'Zincir Restoranlar'  
-                },
-            ]   
-        },
+                    title: 'Sosyal Sorumluluk Projeleri'
+                }
+            ]
+        }
     ]
-   
 
-    return(
+    return (
         <div className="bg-white mt-10">
-            <div className="container mx-auto">
-                <div className="grid grid-cols-4 pt-10">
+            <div className="container mx-auto px-4 md:px-0">
+                <div className="grid gap-y-6 md:grid-cols-2 lg:grid-cols-4 pt-5 md:pt-10">
                     <section>
-                        <nav className="grid gap-y-4 ">
-                        <h6 className="text-lg text-primary-brand-color">Getir'i indirin!</h6>
+                        <h6 className="text-lg text-primary-brand-color mb-4">Getir'i indirin!</h6>
+                        <nav className="grid gap-2 md:gap-4 grid-cols-2 md:grid-cols-1">
                             <a href="#">
-                                <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg"/>
+                                <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" />
                             </a>
                             <a href="#">
-                                <img src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg"/>
+                                <img src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg" />
                             </a>
                             <a href="#">
-                                <img src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg"/>
+                                <img src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg" />
                             </a>
                         </nav>
                     </section>
-                    {menus.map((menu,index) => <Menu key={index} {...menu} />)}
+                    {menus.map((menu, index) => <Menu key={index} {...menu} />)}
                 </div>
-                <div className="flex justify-between items-center border-t border-gray-100 mt-6 py-6">
+                <div className="flex flex-col md:flex-row gap-y-4 justify-between items-center border-t border-gray-100 mt-6 py-6">
                     <div className="text-xs text-gray-700 flex gap-x-8">
-                        &copy; 2023 Getir
-                        <a href="#" className="text-primary-brand-color hover:underline">
-                        Bilgi Toplumu Hizmetleri
+                        &copy; 2021 Getir
+                        <a href="#" className="text-primary-brand-color hover:underline relative before:w-[3px] before:h-[3px] before:bg-black before:absolute before:-left-4 before:top-1/2 before:-translate-y-1/2 before:rounded-full">
+                            Bilgi Toplumu Hizmetleri
                         </a>
                     </div>
-                    <nav className="flex  gap-x-3">
+                    <nav className="flex gap-x-3">
                         <a href="#" className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center">
-                            <FaFacebook size={21}/>
+                            <FaFacebook size={21} />
                         </a>
                         <a href="#" className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center">
-                            <FaTwitter size={21}/>
+                            <FaTwitter size={21} />
                         </a>
                         <a href="#" className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center">
-                            <FaInstagram size={21}/>
+                            <FaInstagram size={21} />
                         </a>
                         <a href="#" className="h-8 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 hover:text-primary-brand-color hover:border-transparent flex items-center px-2 text-sm gap-x-2 rounded text-gray-500 border border-gray-100">
                             <BiGlobe size={18} />
                             Türkçe (TR)
-                        </a> 
+                        </a>
                     </nav>
                 </div>
             </div>
