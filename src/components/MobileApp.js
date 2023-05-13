8a63224f28220d6d611ca75d14e2aed028ac41fb
eren kazan <erennkazan@gmail.com>
2023-05-13 19:04:53 +0300

    card js responsive fixed

--- src/components/MobileApp.js
+++ src/components/MobileApp.js
@@ -1,6 +1,6 @@
 export default function MobileApp(){
     return(
-        <div className="bg-primary-brand-color flex flex-col md:flex-row justify-between items-center bg-mobile-app  rounded-lg text-white">
+        <div className="bg-primary-brand-color flex flex-col md:flex-row justify-between items-center bg-mobile-app  md:rounded-lg text-white">
             <div className="flex flex-col gap-y-3 p-10 text-center md:text-left">
                 <h3 className="text-2xl font-bold tracking-tight">Getir'i indirin</h3>
                 <p className="font-semibold ">İstediğiniz ürünleri dakikalar içinde kapınıza <br/> getirelim.</p>
